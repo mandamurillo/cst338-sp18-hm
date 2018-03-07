ca355b421a3e8cac1beacbf690c4e1d84b11ebdd
MariaEAndaMurillo <manda-murillo@csumb.edu>
2018-03-07 23:47:21 +0000

    pushed origin master

---
+++ inc/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title> Full Statck Development</title>
+         
+        <style>
+            @import url("/cst336/cst338-sp18-hm/css/main.css");
+        </style>
+        
+        
+        
+    </head>
+    <body>
+    <div id="wrapper">
+        <h1 style= "color:black; background-color:gray"> Full Stack Development </h1>
+        <nav>
+        <a href="index.php" id="mainPage">Main Page</a>
+        <a href="info.html">Information</a>
+        <a href= "why.html" >Pros/Cons</a>
+
+        <a href= "how.html">Resources</a>
+        </nav>
+           
+       
+ 
+    
+    <main>
+    <h3>Full Stack Development</h3>
+    <p>
+    Full-stack developers are comfortable working with both back-end and front-end technologies
+    To be specific, it means that the developer can work with databases,<strong>PHP, HTML, CSS, JavaScript </strong>
+    and everything in between,can be anywhere between converting Photoshop desings to front-end code.
+    </p>
+        
+    <figure>
+        <!-- This below did not work :(, had to use the image on the internet
+      <img src="img/stack.png" id="firstImage" alt="Full Stack Development" width="250"> -->
+      <img src="http://blog.debugme.eu/wp-content/uploads/2016/01/Cover-Image.png" id ="firstImage" alt="some_text" style="width:200;height:200;">
+    </figure>
+  
+    
+    
+    </main>
+        
+        
+        
+        
+        
+        
+        </div>
+    </body>
+    
+    
+   <footer>
+      
+
+     <small>2018 &copy; Unknow </small><br />
+     <small>Disclaimer: All material above is used for teaching purposes. Information might be inaccurate.</small>
+     <br />
+     <!-- Following image is using an external site, we shouldn't use external images -->
+     <!--
+     <img src="https://ilearn.csumb.edu/pluginfile.php/443283/course/section/96802/csumb-logo.png" />
+     -->
+     sources:https://www.sitepoint.com/full-stack-developer/
+     
+</footer>   
